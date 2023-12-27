a742c98dbb32cad52f5ae90268893d2843248636
Agustincito <agus@poap.io>
2023-12-27 13:41:54 -0300

    Fix Registry tests

--- test/Registry.test.ts
+++ test/Registry.test.ts
@@ -1,7 +1,6 @@
 import { ethers } from 'hardhat';
-import { time } from '@nomicfoundation/hardhat-network-helpers';
 import {
-  Registry,
+  RegistryV1,
   AlwaysTrueAuthorizer,
   AlwaysFalseAuthorizer,
   PublicListVerifier,
@@ -12,16 +11,16 @@ import { ADD_AUTHORIZER_ROLE } from './utils/roles';
 
 const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
 const ALWAYS_TRUE_AUTHORIZER_ID = 1;
-const ALWAYS_TRUE_TTL = 50000;
 const ALWAYS_FALSE_AUTHORIZER_ID = 2;
 const DOMAIN = 'secureci.xyz';
-const DOMAIN_WITH_WILDCARD = '*.secureci.xyz';
-const CHAIN_ID = 1;
+const DOMAIN_HASH = '0x77ebf9a801c579f50495cbb82e12145b476276f47b480b84c367a30b04d18e15';
+const DOMAIN_WITH_WILDCARD_HASH =
+  '0x1716343d0689cbd485fdf69796462e95bb6ff7a1249660b9fcf2fdd6c6c04f0e';
 
-describe('Registry', function () {
+describe.only('RegistryV1', function () {
   let owner: HardhatEthersSigner;
   let addresses: HardhatEthersSigner[];
-  let registry: Registry;
+  let registry: RegistryV1;
   let alwaysTrueAuthorizer: AlwaysTrueAuthorizer;
   let alwaysFalseAuthorizer: AlwaysFalseAuthorizer;
   let publicListverifier: PublicListVerifier;
@@ -29,13 +28,15 @@ describe('Registry', function () {
   beforeEach(async () => {
     [owner, ...addresses] = await ethers.getSigners();
 
-    const RegistryFactory = await ethers.getContractFactory('Registry');
-    registry = await RegistryFactory.deploy();
+    const NameHashFactory = await ethers.getContractFactory('NameHash');
+    const nameHash = await NameHashFactory.deploy();
+
+    const RegistryFactory = await ethers.getContractFactory('RegistryV1');
+    registry = await RegistryFactory.deploy(await nameHash.getAddress());
     await registry.grantRole(ADD_AUTHORIZER_ROLE, owner.address);
 
     const AlwaysTrueAuthorizer = await ethers.getContractFactory('AlwaysTrueAuthorizer');
     alwaysTrueAuthorizer = await AlwaysTrueAuthorizer.deploy();
-    await alwaysTrueAuthorizer.setTtl(ALWAYS_TRUE_TTL);
     await registry.addAuthorizer(ALWAYS_TRUE_AUTHORIZER_ID, alwaysTrueAuthorizer);
 
     const AlwaysFalseAuthorizer = await ethers.getContractFactory('AlwaysFalseAuthorizer');
@@ -63,23 +64,35 @@ describe('Registry', function () {
   });
 
   describe('Registering domains', function () {
+    it('Should throw an event', async function () {
+      const domainOwner = addresses[0];
+      await expect(
+        registry
+          .connect(domainOwner)
+          .registerDomain(ALWAYS_TRUE_AUTHORIZER_ID, domainOwner, DOMAIN, false),
+      )
+        .to.emit(registry, 'DomainRegistered')
+        .withArgs(ALWAYS_TRUE_AUTHORIZER_ID, domainOwner.address, DOMAIN_HASH, DOMAIN);
+    });
+
     it('Should register a domain successfully', async function () {
       const domainOwner = addresses[0];
-      await registry.connect(domainOwner).registerDomain(ALWAYS_TRUE_AUTHORIZER_ID, DOMAIN, false);
-      expect(await registry.domainToRecord(DOMAIN)).to.deep.equal([
+      await registry
+        .connect(domainOwner)
+        .registerDomain(ALWAYS_TRUE_AUTHORIZER_ID, domainOwner, DOMAIN, false);
+      expect(await registry.domainHashToRecord(DOMAIN_HASH)).to.deep.equal([
         domainOwner.address,
-
-        (await time.latest()) + ALWAYS_TRUE_TTL,
         ZERO_ADDRESS,
       ]);
     });
 
     it('Should register a domain with wildcard successfully', async function () {
       const domainOwner = addresses[0];
-      await registry.connect(domainOwner).registerDomain(ALWAYS_TRUE_AUTHORIZER_ID, DOMAIN, true);
-      expect(await registry.domainToRecord(DOMAIN_WITH_WILDCARD)).to.deep.equal([
+      await registry
+        .connect(domainOwner)
+        .registerDomain(ALWAYS_TRUE_AUTHORIZER_ID, domainOwner, DOMAIN, true);
+      expect(await registry.domainHashToRecord(DOMAIN_WITH_WILDCARD_HASH)).to.deep.equal([
         domainOwner.address,
-        (await time.latest()) + ALWAYS_TRUE_TTL,
         ZERO_ADDRESS,
       ]);
     });
@@ -87,85 +100,12 @@ describe('Registry', function () {
     it("Shouldn't register a domain if the authorizer returns false", async function () {
       const notDomainOwner = addresses[0];
       expect(
-        registry.connect(notDomainOwner).registerDomain(ALWAYS_FALSE_AUTHORIZER_ID, DOMAIN, true),
+        registry
+          .connect(notDomainOwner)
+          .registerDomain(ALWAYS_FALSE_AUTHORIZER_ID, notDomainOwner, DOMAIN, true),
       )
         .revertedWithCustomError(registry, 'AccountIsNotAuthorizeToRegisterDomain')
         .withArgs(notDomainOwner.address, DOMAIN);
-    });
-  });
-
-  describe('Domain ttl', function () {
-    let domainOwner: HardhatEthersSigner;
-    let domainTtl: number;
-
-    beforeEach(async () => {
-      domainOwner = addresses[0];
-      await registry.connect(domainOwner).registerDomain(ALWAYS_TRUE_AUTHORIZER_ID, DOMAIN, false);
-      domainTtl = (await time.latest()) + ALWAYS_TRUE_TTL;
-    });
-
-    describe('Is Domain Valid', function () {
-      it('Should return false if not domain was registered', async function () {
-        expect(await registry.isDomainValid('another.domain.com')).to.false;
-      });
-
-      it('Should return false if the domain ttl expired', async function () {
-        await time.increase(ALWAYS_TRUE_TTL + 1);
-        expect(await registry.isDomainValid(DOMAIN)).to.false;
-      });
-
-      it('Should return true if the domain ttl is not expired', async function () {
-        expect(await registry.isDomainValid(DOMAIN)).to.true;
-        await time.increase(ALWAYS_TRUE_TTL - 1);
-        expect(await registry.isDomainValid(DOMAIN)).to.true;
-      });
-
-      it('Should return the correct ttl', async function () {
-        expect(await registry.domainTtl(DOMAIN)).to.equal(domainTtl);
-      });
-
-      it('Should return the correct ttl event if it expired', async function () {
-        await time.increase(ALWAYS_TRUE_TTL + 1);
-        expect(await registry.domainTtl(DOMAIN)).to.equal(domainTtl);
-      });
-    });
-
-    describe('Domain Owner', function () {
-      let domainOwner: HardhatEthersSigner;
-      let notDomainOwner: HardhatEthersSigner;
-
-      beforeEach(async () => {
-        domainOwner = addresses[0];
-        notDomainOwner = addresses[1];
-        await registry
-          .connect(domainOwner)
-          .registerDomain(ALWAYS_TRUE_AUTHORIZER_ID, DOMAIN, false);
-      });
-
-      it('Should return false if the domain is not registered', async function () {
-        expect(await registry.isDomainOwner('another.domain.com', domainOwner.address)).to.false;
-      });
-
-      it('Should return false if it is not the domain owner', async function () {
-        expect(await registry.isDomainOwner(DOMAIN, notDomainOwner.address)).to.false;
-      });
-
-      it('Should return true if it is the domain owner', async function () {
-        expect(await registry.isDomainOwner(DOMAIN, domainOwner.address)).to.true;
-      });
-
-      it('Should return the domain owner if it is a valid domain', async function () {
-        expect(await registry.domainOwner(DOMAIN)).to.equal(domainOwner.address);
-      });
-
-      it('Should return the zero address if it was not register', async function () {
-        expect(await registry.domainOwner('another.domain.com')).to.equal(ZERO_ADDRESS);
-      });
-
-      it('Should return the zero address if the domain expired', async function () {
-        await time.increase((await registry.domainTtl(DOMAIN)) + BigInt(1));
-        expect(await registry.domainOwner(DOMAIN)).to.equal(ZERO_ADDRESS);
-      });
     });
   });
 
@@ -176,16 +116,20 @@ describe('Registry', function () {
     beforeEach(async () => {
       domainOwner = addresses[0];
       notDomainOwner = addresses[1];
-      await registry.connect(domainOwner).registerDomain(ALWAYS_TRUE_AUTHORIZER_ID, DOMAIN, false);
+      await registry
+        .connect(domainOwner)
+        .registerDomain(ALWAYS_TRUE_AUTHORIZER_ID, domainOwner, DOMAIN, false);
     });
 
     it('Should only let the owner of the domain add a verifier', async function () {
-      await registry.connect(domainOwner).addVerifier(DOMAIN, publicListverifier.target);
-      expect(await registry.domainVerifier(DOMAIN)).to.equal(publicListverifier.target);
+      await registry.connect(domainOwner).addVerifier(DOMAIN_HASH, publicListverifier.target);
+      expect(await registry.domainVerifier(DOMAIN_HASH)).to.equal(publicListverifier.target);
 
-      await expect(registry.connect(notDomainOwner).addVerifier(DOMAIN, publicListverifier.target))
+      await expect(
+        registry.connect(notDomainOwner).addVerifier(DOMAIN_HASH, publicListverifier.target),
+      )
         .revertedWithCustomError(registry, 'AccountIsNotDomainOwner')
-        .withArgs(notDomainOwner.address, DOMAIN);
+        .withArgs(notDomainOwner.address, DOMAIN_HASH);
     });
   });
 });
