d4f573f49b5ead205c346ad0062d2f8e3c3e91cc
Agustincito <agus@poap.io>
2023-12-29 19:12:32 -0300

    Add SCI Authorizer tests

--- utils/roles.ts
+++ utils/roles.ts
@@ -1,5 +1,7 @@
 import { ethers } from 'hardhat';
 
 export const ADD_AUTHORIZER_ROLE = ethers.keccak256(Buffer.from('ADD_AUTHORIZER_ROLE'));
+
+export const IS_AUTHORIZED = ethers.keccak256(Buffer.from('IS_AUTHORIZED'));
 export const DEFAULT_ADMIN_ROLE =
   '0x0000000000000000000000000000000000000000000000000000000000000000';
