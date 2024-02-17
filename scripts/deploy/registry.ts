78f27d99ab05d36feff2779676814f3d6e31e981
Agustincito <agus@poap.io>
2024-02-17 20:09:32 -0300

    Add grant role to registry

--- scripts/deploy/registry.ts
+++ scripts/deploy/registry.ts
@@ -1,11 +1,12 @@
 import { ethers } from 'hardhat';
+import { ADD_AUTHORIZER_ROLE } from '../../utils/roles';
 
 const NAME_HASH_ADDRESS = '0x65fc5b21BDd27726510F554b62E60e3cAA9B2C9A';
 const SCI_AUTHORIZER = '0xe1da59CB197D6944ABDfC6D37CbDd20FaD567dc7';
 async function main() {
   const RegistryFactory = await ethers.getContractFactory('Registry');
   const registry = await RegistryFactory.deploy(NAME_HASH_ADDRESS);
-  // TODO: Add grant
+  await registry.grantRole(ADD_AUTHORIZER_ROLE, await ethers.provider.getSigner());
   await registry.setAuthorizer(1, SCI_AUTHORIZER);
   console.log(`Deployed Registry to ${registry.target}`);
 }
