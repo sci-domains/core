6b7e8f79f4c35a829970252a77ff3c61a9a140dc
Agustincito <agus@poap.io>
2024-03-24 20:12:49 -0300

    Fix hardhat envs

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,6 +4,14 @@ import '@nomiclabs/hardhat-solhint';
 import '@openzeppelin/hardhat-upgrades';
 import 'dotenv/config';
 
+function getAccounts(privateKey: string | undefined): string[] {
+  return privateKey ? [privateKey] : [];
+}
+
+function getUrl(url: string | undefined): string {
+  return url ?? '';
+}
+
 const config: HardhatUserConfig = {
   solidity: '0.8.20',
   gasReporter: {
@@ -16,8 +24,8 @@ const config: HardhatUserConfig = {
   networks: {
     goerli: {
       chainId: 5,
-      url: process.env.ETHEREUM_GOERLI_PROVIDER_URL!,
-      accounts: [process.env.ETHEREUM_GOERLI_PRIVATE_KEY!],
+      url: getUrl(process.env.ETHEREUM_GOERLI_PROVIDER_URL),
+      accounts: getAccounts(process.env.ETHEREUM_GOERLI_PRIVATE_KEY),
     },
   },
   etherscan: {
