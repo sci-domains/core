adab1008ee0f2f68af347a501dbaf30fadd1d713
Agustincito <agus@poap.io>
2024-10-11 10:13:07 -0300

    Delete non upgreadable deployment scripts

--- scripts/utils.ts
+++ scripts/utils.ts
@@ -1,60 +1,7 @@
 import { ethers, network } from 'hardhat';
-import { BaseContract } from 'ethers';
-import fs from 'fs';
 import Safe from '@safe-global/protocol-kit';
 import SafeApiKit from '@safe-global/api-kit';
 import { ContractTransaction } from 'ethers/lib.esm';
-
-export enum CONTRACT_NAMES {
-  REGISTRY = 'Registry',
-  NAME_HASH = 'NameHash',
-  PUBLIC_LIST_VERIFIER = 'PublicListVerifier',
-  SCI = 'SCI',
-  SCI_AUTHORIZER = 'SCIAuthorizer',
-}
-
-const DEPLOYMENT_FOLDER = './deployments';
-
-async function generateDeploymentPath(): Promise<string> {
-  const network = await ethers.provider.getNetwork();
-  return `${DEPLOYMENT_FOLDER}/${network.name}`;
-}
-
-export async function saveDeployment(contract: BaseContract, contractName: string) {
-  const filePath = await generateDeploymentPath();
-
-  // Create the directory if it doesn't exist
-  if (!fs.existsSync(filePath)) {
-    fs.mkdirSync(filePath);
-  }
-
-  fs.writeFileSync(
-    `${filePath}/${contractName}.json`,
-    JSON.stringify(
-      {
-        address: contract.target,
-      },
-      null,
-      2,
-    ),
-  );
-}
-
-export async function logDeployment(contract: BaseContract, contractName: string, args: string[]) {
-  console.log(`Deployed ${contractName} to ${contract.target}`);
-  const network = await ethers.provider.getNetwork();
-  console.log(
-    `To verify it run:\nyarn hardhat verify --network ${network.name} ${
-      contract.target
-    } ${args.join(' ')}`,
-  );
-}
-
-export async function getDeployedContractAddress(contractName: string): Promise<string> {
-  const filePath = await generateDeploymentPath();
-  const deploymentFile = JSON.parse(fs.readFileSync(`${filePath}/${contractName}.json`).toString());
-  return deploymentFile.address;
-}
 
 export async function sendTransactionsViaSafe(
   ethersTransactions: ContractTransaction[],
