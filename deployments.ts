da309789688e152b17dad92f2ebc4bcf60f9b397
Agustincito <agus@poap.io>
2024-12-26 13:32:06 -0300

    Add release workflow

--- deployments.ts
+++ deployments.ts
@@ -1,11 +1,11 @@
 export const deployments = {
-  "11155111": {
-    "SciRegistry#SciRegistry": "0xaDD75Aecd98f0ADAD899729c88BfED4f8951c02A",
-    "PublicListVerifier#PublicListVerifier": "0xd58d48185146343720df2C26FcD8D3C3734e22cf",
-    "SciRegstrar#SciRegistrar": "0x83223D23E769CFE4b8bec8A32Eb148d0dbEc4dE5",
-    "ProxyModule#SCI": "0xC29e83AfEe01bDccA7f69fE1DfD7A61954dCE4d4",
-    "ProxyModule#TransparentUpgradeableProxy": "0x1694F779f28E3B3ff409d62c9d9d7042Ff406aEC",
-    "ProxyModule#ProxyAdmin": "0x9007eD4FFa791BB63662D6E894A97e163e4C2A23",
-    "SciModule#SCI": "0x1694F779f28E3B3ff409d62c9d9d7042Ff406aEC"
-  }
+  '11155111': {
+    'SciRegistry#SciRegistry': '0xaDD75Aecd98f0ADAD899729c88BfED4f8951c02A',
+    'PublicListVerifier#PublicListVerifier': '0xd58d48185146343720df2C26FcD8D3C3734e22cf',
+    'SciRegstrar#SciRegistrar': '0x83223D23E769CFE4b8bec8A32Eb148d0dbEc4dE5',
+    'ProxyModule#SCI': '0xC29e83AfEe01bDccA7f69fE1DfD7A61954dCE4d4',
+    'ProxyModule#TransparentUpgradeableProxy': '0x1694F779f28E3B3ff409d62c9d9d7042Ff406aEC',
+    'ProxyModule#ProxyAdmin': '0x9007eD4FFa791BB63662D6E894A97e163e4C2A23',
+    'SciModule#SCI': '0x1694F779f28E3B3ff409d62c9d9d7042Ff406aEC',
+  },
 };
